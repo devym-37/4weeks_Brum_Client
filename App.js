39a832232c988fea0eee0bdbf3418581076f129d
Joeun <kimgood91@gmail.com>
2019-11-18 01:48:21 +0900

    Preload assets

--- App.js
+++ App.js
@@ -1,19 +1,41 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import * as Font from "expo-font";
+import { Asset } from "expo-asset";
+import { Ionicons } from "@expo/vector-icons";
+import { AppLoading } from "expo";
 
 export default function App() {
-  return (
+  const [loaded, setLoaded] = useState(false);
+
+  const preLoad = async () => {
+    try {
+      await Font.loadAsync({ ...Ionicons.font });
+      await Asset.loadAsync([require("./assets/logo.png")]);
+      setLoaded(true);
+    } catch (e) {
+      console.log(`preLoad error: `, e);
+    }
+  };
+
+  useEffect(() => {
+    preLoad();
+  }, []);
+
+  return loaded ? (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
     </View>
+  ) : (
+    <AppLoading />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   }
 });
