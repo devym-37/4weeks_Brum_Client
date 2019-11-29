7c454d7767b2719f6a3931a551257ffc77884784
Joeun <kimgood91@gmail.com>
2019-11-29 12:54:42 +0900

    Change filename chatNavigation.js -> ChatNavigation.js

--- App.js
+++ App.js
@@ -21,7 +21,7 @@ import StartNavigation from "./navigation/StartNavigation";
 import MainNavigation from "./navigation/MainNavigation";
 import LoggedOutMainNavigation from "./navigation/LoggedOutMainNavigation";
 
-import ChatNavigation from "./navigation/ChatNavigation";
+// import ChatNavigation from "./navigation/ChatNavigation";
 // Imports: Screens
 
 import Signup from "./screens/Auth/Signup";
