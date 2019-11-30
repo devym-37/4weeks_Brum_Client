8eb971448248c4c3b2a478509faecb0a8497614f
Joeun <kimgood91@gmail.com>
2019-11-30 18:23:38 +0900

    POST data of newOrder on server

--- redux/reducers/orderReducer.js
+++ redux/reducers/orderReducer.js
@@ -2,9 +2,9 @@
 const initialState = {
   title: null,
   category: null,
-  departures: null,
-  arrivals: null,
-  desiredArrival: null,
+  // departures: null,
+  // arrivals: null,
+  desiredArrivalTime: null,
   price: null,
   details: null,
   images: null,
@@ -17,59 +17,70 @@ const orderReducer = (state = initialState, action) => {
   switch (action.type) {
     case "CREATE_CATEGORY": {
       return {
+        ...state,
         category: action.category
       };
     }
     case "CREATE_DEPARTURES": {
       return {
+        ...state,
         departures: action.departures
       };
     }
     case "CREATE_ARRIVALS": {
       return {
+        ...state,
         arrivals: action.arrivals
       };
     }
     case "CREATE_DESIRED_ARRIVAL": {
       return {
-        desiredArrival: action.desiredArrival
+        ...state,
+        desiredArrivalTime: action.desiredArrival
       };
     }
     case "CREATE_PRICE": {
       return {
+        ...state,
         price: action.price
       };
     }
     case "CREATE_DETAILS": {
       return {
+        ...state,
         details: action.details
       };
     }
 
     case "CREATE_TITLE": {
       return {
+        ...state,
         title: action.title
       };
     }
     case "CREATE_ISPRICE": {
       return {
-        isPrice: action.isPrice
+        ...state,
+        isPrice: !state.isPrice
       };
     }
 
     case "CREATE_IMAGES": {
       return {
+        ...state,
         images: action.images
       };
     }
 
     case "SAVE_ORDERID": {
       return {
+        ...state,
         orderId: action.orderId
       };
     }
     case "DELETE_IMAGES": {
       return {
+        ...state,
         images: state.images.pop()
       };
     }
