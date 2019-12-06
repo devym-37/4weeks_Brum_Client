d4c353081cdb8f2d71e5b2588c40d4ec461b5784
DOMO9293 <flqjslgmail.com>
2019-12-06 15:53:16 +0100

    status mod

--- screens/chat/Chat.js
+++ screens/chat/Chat.js
@@ -635,7 +635,7 @@ class Chat extends React.Component {
               <OrderContainer>
                 <GhostButton
                   onPress={() => {
-                    changestatus(88);
+                    changestatus(4);
                     this.props.navigation.navigate("ReviewScreen", {
                       Chat: true
                     });
